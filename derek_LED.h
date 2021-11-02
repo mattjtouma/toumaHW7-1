47ab5cebcb934ce87223d2796579158b397bcd5d
Matthew J. Touma <mattjtouma@gmail.com>
2021-11-02 17:59:09 +0000

    Uploading HW7-1

---
+++ derek_LED.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class LED{
+   private:
+      string path;
+      int number;
+      virtual void writeLED(string filename, string value);
+      virtual void removeTrigger();
+   public:
+      LED(int number);
+      virtual void turnOn();
+      virtual void turnOff();
+      virtual void flash(string delayms);
+      virtual void outputState();
+      virtual ~LED();
+};
